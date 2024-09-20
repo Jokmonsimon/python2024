efef6bcb14c74b32b6edcfb981fa3e16016a81aa
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-09-20 13:39:51 +0300

    Python for DS and ML

---
+++ python_syntax.py
@@ -0,0 +1,18 @@
+# print("500 Days Run"); print("Fast Track Challenge")
+# Create two variables to hold pace for the first and second km
+
+pace_for_1km = 284 # Pace per kilometer in seconds
+pace_for_2km = 244 # Pace per kilometer in seconds
+pace_for_3km = 254 # Pace per kilometer in seconds
+
+# Add the variables
+total_pace = pace_for_1km +            pace_for_2km \
++ pace_for_3km
+
+# Show the total pace
+print(total_pace)
+
+total_challenge_days = 500
+
+if total_challenge_days == 500:
+    print(total_challenge_days)
