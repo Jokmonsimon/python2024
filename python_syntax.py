41043727ec38fce20332c8e77e749ba6c18710cd
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-09-20 14:02:41 +0300

    Update comments

--- python_syntax.py
+++ python_syntax.py
@@ -1,9 +1,9 @@
 # print("500 Days Run"); print("Fast Track Challenge")
 # Create two variables to hold pace for the first and second km
 
-pace_for_1km = 284 # Pace per kilometer in seconds
-pace_for_2km = 244 # Pace per kilometer in seconds
-pace_for_3km = 254 # Pace per kilometer in seconds
+pace_for_1km = 284 # Pace for the first kilometer in seconds
+pace_for_2km = 244 # Pace for the second kilometer in seconds
+pace_for_3km = 254 # Pace for the third kilometer in seconds
 
 # Add the variables
 total_pace = pace_for_1km +            pace_for_2km \
