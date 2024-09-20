d45b4d80312ea1aa3544114383319be83fd7f45e
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-09-20 15:30:18 +0300

    New update

--- python_syntax.py
+++ python_syntax.py
@@ -1,18 +1,23 @@
 print("500 Days Run"); print("Fast Track Challenge")
-# Create two variables to hold pace for the first and second km
 
+
+# Create two variables to hold pace for the first and second km
 pace_for_1km = 284 # Pace for the first kilometer in seconds
 pace_for_2km = 244 # Pace for the second kilometer in seconds
 pace_for_3km = 254 # Pace for the third kilometer in seconds
+
 
 # Add the variables
 total_pace = pace_for_1km +            pace_for_2km \
 + pace_for_3km
 
+
 # Show the total pace
 print(total_pace)
 
+
 total_challenge_days = 500
+
 
 if total_challenge_days == 500:
     print(total_challenge_days)
