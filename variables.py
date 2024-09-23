dde22045bcd7e1886bb680bacaa4493680e0870c
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-09-23 23:04:20 +0300

    Variables in python

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Declaring a variable in Python
+day1_date = "23-Sep-2024"
+day1_start_time = "6:40:00 AM"
+day1_end_time = "6:55:00 AM"
+
+# Printing variable values
+print(day1_date)
+print(day1_start_time)
+print(day1_end_time)
