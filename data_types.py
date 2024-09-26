53455761c38df941c4a710efade05084dcdf3ef4
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-09-26 23:18:50 +0300

    Data types in python

---
+++ data_types.py
@@ -0,0 +1,45 @@
+# # Integer (int) data types
+# timer_day1 = 1251 # seconds
+# timer_day2 = 1248 # seconds
+
+# # Print the integer values
+# print(timer_day1) # Output for day 1
+# print(timer_day2) # Output for day 2
+
+# # Float data type
+# distance_day1 = 5.74
+# distance_day2 = 5.73
+# dis = int(distance_day2)
+
+# # Print the float values
+# print(distance_day1)
+# print("The distance covered on day 2 was:", distance_day2, "km.")
+# print(dis)
+# print(type(distance_day2))
+# print(type(dis))
+
+# # String (str) data type
+# route_day1 = "R1"
+# activity_day1 = 'Running'
+# date_day1 = "19-Nov-2023"
+# start_time_day1 = '6:34:00 AM'
+
+# # Print the string values
+# print(route_day1)
+# print(activity_day1)
+# print(date_day1)
+# print(start_time_day1)
+
+# # Booleans (bool) data type
+# is_running_day1 = True
+# is_time_trial_day1 = False
+
+# # Print the boolean values
+# print(is_running_day1)
+# print(is_time_trial_day1)
+
+challenge_days = 500
+if challenge_days < 500:
+    print("You have not yet completed the challenge")
+else:
+    print("The challenge is completed")
